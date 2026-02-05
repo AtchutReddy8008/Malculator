9d878fa471abe40fa4b105ebe62bdb5f872daf9c
AtchutReddy8008 <Atchutreddy8008@outlook.com>
2026-02-05 15:26:18 +0530

    Initial commit

--- niftybot/trading/core/bot_original.py
+++ niftybot/trading/core/bot_original.py
@@ -41,8 +41,8 @@ class Config:
     EXCHANGE = "NFO"
     UNDERLYING = "NIFTY"
     LOT_SIZE = 65
-    ENTRY_START = dtime(13, 24, 0)
-    ENTRY_END = dtime(13, 24, 0)
+    ENTRY_START = dtime(9, 21, 0)
+    ENTRY_END = dtime(9, 22, 0)
     TOKEN_REFRESH_TIME = dtime(8, 30)
     EXIT_TIME = dtime(15, 0)
     MARKET_CLOSE = dtime(15, 30)
