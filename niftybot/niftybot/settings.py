75fa01fdf0e42736e8f8d221f6cb3ba620aff0f8
AtchutReddy8008 <Atchutreddy8008@outlook.com>
2026-02-21 18:18:43 +0530

    inn commit

--- niftybot/niftybot/settings.py
+++ niftybot/niftybot/settings.py
@@ -137,12 +137,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+import os
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-          # Development: local static files
-STATIC_ROOT = BASE_DIR / 'staticfiles'            # Production: collectstatic destination
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'         # Production: collectstatic destination
 
 # Media files (user uploads - if you add any later)
 MEDIA_URL = '/media/'
