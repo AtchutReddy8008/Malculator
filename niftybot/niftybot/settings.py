9f7d0a9d180f58113844f6c1bd933a154c0b5f98
AtchutReddy8008 <Atchutreddy8008@outlook.com>
2026-02-25 20:41:43 +0530

    k commit

--- niftybot/niftybot/settings.py
+++ niftybot/niftybot/settings.py
@@ -100,23 +100,23 @@ WSGI_APPLICATION = 'niftybot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'tradewithme',
-#         'HOST': 'database-2.ckl6umkmkrui.us-east-1.rds.amazonaws.com',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'tradewithme',
+        'HOST': 'database-2.ckl6umkmkrui.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 #pip install psycopg2-binary
 
 
